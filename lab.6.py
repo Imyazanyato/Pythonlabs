cefd688f9cf8f7a383640a7bc928bd287fe86b9f
Litovchenko Nikita <113112508+Imyazanyato@users.noreply.github.com>
2022-12-24 18:45:40 +0300

    Update lab.6.py

--- lab.6.py
+++ lab.6.py
@@ -6,16 +6,15 @@ bot = telebot.TeleBot('5699152262:AAHLwYNXA4hgiWRfrBK3pmrEzN8rQ8ABLMQ')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет! Хочешь узнать какой нибудь интересный факт? Чтобы узнать, как это сделать, напиши мне /help')
+    bot.send_message(message.chat.id, 'Привет! Хочешь новые обои на телефон? Чтобы узнать, как это сделать, напиши мне /help')
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, 'этот бот присылает тебе случайную дату и интересный факт, который произошел в этот день. Для того, чтобы попробовать, напиши /data')
+    bot.send_message(message.chat.id, 'этот бот присылает тебе случайную картинку. Для того, чтобы попробовать, напиши /go')
 
-@bot.message_handler(commands=['data'])
+@bot.message_handler(commands=['go'])
 def data(message):
-    r = requests.get('http://numbersapi.com/random/date')
-    url = r.url
-    bot.send_message(message.chat.id, url)    
+    r = requests.get('https://random.imagecdn.app/900/1600')
+    bot.send_photo(message.chat.id, r.url)
 
 bot.polling(none_stop=True)
